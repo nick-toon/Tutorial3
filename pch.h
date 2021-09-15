6f0037ae06a97658e0deb11cae236429bbb743c7
nick-toon <62121599+nick-toon@users.noreply.github.com>
2021-09-14 22:55:23 -0400

    Add project files.

---
+++ pch.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+
+#include <winrt/base.h>
+
+#pragma comment(lib, "windowsapp")
+
+
